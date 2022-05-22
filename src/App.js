8485f031443b6cd5d98f561049096ec33cdb6b2f
Arafath-Rahman <mkhan172093@bscse.uiu.ac.bd>
2022-05-23 00:34:35 +0600

    project structure added, start designing home page, started with navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
+import Navbar from './Pages/Shared/Navbar';
 
 function App() {
   return (
     <div>
-      <h1 className='text-3xl font-bold text-emerald-400'>Hello</h1>
-      <button className="btn btn-primary">Button</button>
+      <Navbar />
     </div>
   );
 }
---
+++ src/Pages/Shared/Navbar.js
@@ -0,0 +1,79 @@
+import React from "react";
+
+const Navbar = () => {
+  const menuItems = ["Home", "Parts", "Reviews"];
+  return (
+    <div className="lg:px-12">
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex="0" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex="0"
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              {menuItems.map((item) => (
+                <li key={item}>
+                  <a>{item}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <a className="btn btn-ghost normal-case text-xl">
+            Robotics Parts Store
+          </a>
+        </div>
+
+        <div className="navbar-end">
+          <div className="hidden lg:flex">
+            <ul className="menu menu-horizontal p-0">
+              {menuItems.map((item) => (
+                <li key={item}>
+                  <a>{item}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="dropdown dropdown-end">
+            <label tabIndex="0" className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full">
+                <img
+                  src="https://api.lorem.space/image/face?hash=33791"
+                  alt="avatar"
+                />
+              </div>
+            </label>
+            <ul
+              tabIndex="0"
+              className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a className="justify-between">Profile</a>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
